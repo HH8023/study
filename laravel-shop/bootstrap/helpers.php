89ce71f99fc0a9bfbfc52cdb860d13a7ba44723d
wandx <wp_0357@163.com>
2018-11-15 01:01:05 +0800

    基础布局

--- laravel-shop/bootstrap/helpers.php
+++ laravel-shop/bootstrap/helpers.php
@@ -4,7 +4,20 @@
  *
 存放所有自定义辅助函数 * */
 
-
-function test_helper() {
+//测试函数
+/*function test_helper() {
     return 'OK';
+}*/
+
+/*
+ * 会把当前请求的路由名称转换为css类名称，作用是允许我们针对某个页面做页面样式定制
+ * */
+function route_class()
+{
+    return str_replace('.','-',Route::currentRouteName());
 }
+
+
+
+
+
---
+++ laravel-shop/resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title','Laravel Shop') - wandx小店</title>
+
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app" class="{{ route_class() }}--page">
+        @include('layouts._header')
+        <div class="container">
+            @yield('content')
+        </div>
+        @include('layouts._footer')
+    </div>
+
+    {{--JS脚本--}}
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
