a3bba27ddf63673b6af0a5fdd4fc36afdd72c203
wandx <wp_0357@163.com>
2018-12-04 23:20:16 +0800

    验证邮箱中间件3.2over

--- laravel-shop/routes/web.php
+++ laravel-shop/routes/web.php
@@ -17,3 +17,13 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/email_verify_notic', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+
+    //开始
+    Route::group(['middleware' => 'email_verified'], function () {
+        Route::get('/test', function() {
+            return 'Your email is verified';
+        });
+    });
+});
