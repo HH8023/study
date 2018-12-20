2868dfc38ca504b15e07f883f59c2f8ccac1caaa
wangpeng <wangpeng@sinovine.com>
2018-12-20 18:03:30 +0800

    访问单个商品5.6.5over

--- laravel-shop/database/factories/ProductSkuFactory.php
+++ laravel-shop/database/factories/ProductSkuFactory.php
@@ -4,10 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\ProductSku::class, function (Faker $faker) {
     return [
-        //
-        'title' => $faker->word,
+        'title'        => $faker->word,
         'description' => $faker->sentence,
-        'price' => $faker->randomNumber(4),
-        'stock' => $faker->randomNumber(5),
+        'price'        => $faker->randomNumber(4),
+        'stock'        => $faker->randomNumber(5),
     ];
 });
