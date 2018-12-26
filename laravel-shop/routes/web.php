54210230d69428459606a4cd2646ba35e63edf61
wandx <wp_0357@163.com>
2018-12-26 23:57:23 +0800

    添加商品到购物车6.1over

--- laravel-shop/routes/web.php
+++ laravel-shop/routes/web.php
@@ -36,6 +36,7 @@ Route::group(['middleware' => 'auth'], function () {
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
         Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');  //收藏商品列表
+        Route::post('cart', 'CartController@add')->name('cart.add'); //添加商品到购物车
 
 
 
