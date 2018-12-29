0f866e1cfc62ab1bc48bc7a73efb2ec4802a0278
wandx <wp_0357@163.com>
2018-12-29 23:44:25 +0800

    订单模型6.3over 有bug

--- laravel-shop/database/migrations/2018_12_28_232050_create_order_items_table.php
+++ laravel-shop/database/migrations/2018_12_28_232050_create_order_items_table.php
@@ -15,17 +15,17 @@ class CreateOrderItemsTable extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('ordeer_id');
+            $table->unsignedInteger('order_id');
             $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
             $table->unsignedInteger('product_id');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->unsignedInteger('product_sku_id');
             $table->foreign('product_sku_id')->references('id')->on('product_skus')->onDelete('cascade');
             $table->unsignedInteger('amount');
-            $table->decimal('price',10,2);
+            $table->decimal('price', 10, 2);
             $table->unsignedInteger('rating')->nullable();
             $table->text('review')->nullable();
-            $table->timestamps('reviewed_at')->nullable();
+            $table->timestamp('reviewed_at')->nullable();
         });
     }
 
