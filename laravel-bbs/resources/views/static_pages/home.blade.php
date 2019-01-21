ad8517ef75e087b4066885714d45850cfcb3f93c
wangpeng <wangpeng@sinovine.com>
2019-01-21 17:04:58 +0800

    初始化的样式4.2

--- laravel-bbs/resources/views/static_pages/home.blade.php
+++ laravel-bbs/resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
+
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Wandx</h1>
+        <p class="lead">
+            您现在所看到的是 <a href="">wandx - BBS</a> 的测试博客
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
