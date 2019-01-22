75792937d31e9704ccc90570253d0ce96ae6ff7c
wangpeng <wangpeng@sinovine.com>
2019-01-22 15:32:43 +0800

    增加models文件夹存放model

--- laravel-bbs/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel-bbs/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Models;
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -26,7 +28,7 @@ class CreateUsersTable extends Migration
 
     /**
      * Reverse the migrations.
-     *
+     * 回滚迁移
      * @return void
      */
     public function down()
