a7112772cf08bb76aebfeb6b12b9d61f78441f94
wangpeng <wangpeng@sinovine.com>
2019-02-13 15:28:24 +0800

    查看用户列表--模型工厂有bug，手动添加用户（在tinker中）

--- laravel-bbs/app/Http/Controllers/UsersController.php
+++ laravel-bbs/app/Http/Controllers/UsersController.php
@@ -12,12 +12,19 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth',[
-            'except' => ['show','create','store']
+            'except' => ['show','create','store','index']
         ]);
 
         $this->middleware('guest',[
             'only' => ['create']
         ]);
+    }
+
+    //
+    public function index()
+    {
+        $users = User::paginate(5);
+        return view('users.index',compact('users'));
     }
 
     //新建
--- laravel-bbs/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel-bbs/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->default('123456789');
             $table->rememberToken();
             $table->timestamps();
         });
